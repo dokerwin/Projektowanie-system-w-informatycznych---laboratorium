821646e9d9e1c34f1fb831098e4e29ed1ae0b2f0
unknown <nik.name.doker@gmail.com>
2021-03-27 10:05:57 +0100

    Factory Method

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+
+
+    abstract class Furniture
+
+    {
+    }
+
+
+
+    class Chair : Furniture
+
+    {
+    }
+
+
+
+    class ArmChair : Furniture
+
+    {
+    }
+
+
+    abstract class CreatorFurniture
+
+    {
+        public abstract Furniture FactoryMethod();
+    }
+
+
+    class ChairCreator : CreatorFurniture
+
+    {
+        public override Furniture FactoryMethod()
+        {
+            return new Chair();
+        }
+    }
+
+    class ArmChairCreator : CreatorFurniture
+
+    {
+        public override Furniture FactoryMethod()
+        {
+            return new ArmChair();
+        }
+    }
+
+
+
+
+    class MainApp
+
+    {
+   
+        static void Main()
+        {
+          
+
+            CreatorFurniture[] creators = new CreatorFurniture[2];
+
+            creators[0] = new ChairCreator();
+            creators[1] = new ArmChairCreator();
+
+
+            foreach (CreatorFurniture creator in creators)
+            {
+                Furniture furniture  = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                  furniture.GetType().Name);
+            }
+
+  
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
