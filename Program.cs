1ec681dcfe9ec17370c44fce9b6cc5f961dd90da
dokerwin <70201621+dokerwin@users.noreply.github.com>
2021-04-03 13:42:11 +0200

    Adapter

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _36350Adapter
+{ 
+
+    public interface ITarget
+    {
+        int GetRequest();
+    }
+
+    class Adaptee
+    {
+        public double GetSpecificRequest()
+        {
+            return 3.14;
+        }
+    }
+
+  
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public int GetRequest()
+        {
+            int a = (int)_adaptee.GetSpecificRequest();
+            return a;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+            Console.WriteLine("Class adaptee returns double : " + adaptee.GetSpecificRequest());
+            Console.WriteLine("By adapter i have converted double to int : "+ target.GetRequest());
+       
+            
+        }
+    }
+}
+
